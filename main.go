bc8158b4cb0d6abe3dfae093acd1e5ce20d2a773
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-06-22 06:56:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"quantum-blockchain/blockchain"
+	"quantum-blockchain/rpc"
+	"syscall"
 )
 
 func main() {
+	dataDir := flag.String("datadir", "./qdata", "Data directory for blockchain")
+	rpcPort := flag.String("rpcport", "8545", "RPC server port")
+	validator := flag.Bool("validator", false, "Run as validator node")
+	flag.Parse()
+
 	fmt.Println("🟣 Quantum Blockchain v1.0")
 	fmt.Println("Creator: BilliStrom | 22.06.2025")
 
-	bc := blockchain.InitBlockchain()
-	defer bc.Database.Close()
+	bc, err := blockchain.InitBlockchain(*dataDir)
+	if err != nil {
+		log.Fatalf("Failed to initialize blockchain: %v", err)
+	}
+	defer bc.Close()
 
-	// Создание кошельков
-	walletA := blockchain.NewWallet()
-	walletB := blockchain.NewWallet()
+	// Пример добавления транзакций
+	if *validator {
+		log.Println("🔐 Running in validator mode")
+		wallet := blockchain.NewWallet()
+		bc.AddStake(wallet.Address(), big.NewInt(1000))
+	}
 
-	// Добавление стейка
-	bc.AddStake(walletA.Address(), big.NewInt(1000))
-	bc.AddStake(walletB.Address(), big.NewInt(500))
+	// Запуск RPC сервера
+	go rpc.Start(*rpcPort, bc)
 
-	// Генерация транзакций
-	tx := blockchain.NewTransaction(walletA, walletB.Address(), 50)
-	bc.AddBlock([]*blockchain.Transaction{tx})
-
-	log.Println("✅ Blockchain запущен!")
+	// Обработка сигналов для корректного завершения
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	log.Println("🛑 Shutting down...")
 }
